30935f50bc368214fb95b280444996872b94c3d4
gilangFaras <gf.arifah@gmail.com>
2025-05-02 15:08:04 +0700

    Random Scale animation done

--- src/randomScale/characters.tsx
+++ src/randomScale/characters.tsx
@@ -1,20 +1,62 @@
-import {Text, Animated, StyleSheet} from 'react-native';
-import React from 'react';
+import {Text, Animated, StyleSheet, Easing} from 'react-native';
+import React, {useCallback, useEffect, useRef} from 'react';
 
-const Characters = (props: {character: string}) => {
-  const {character} = props;
+const Characters = (props: {
+  character: string;
+  linesLength: number;
+  currentLine: number;
+}) => {
+  const {character, linesLength, currentLine} = props;
+  const scaleAnimation = useRef(new Animated.Value(0)).current;
+  const opacityAnimation = useRef(new Animated.Value(1)).current;
+
+  // Function to generate random number for animation in delay
+  const randomNumber = (max: number) => {
+    return Math.floor(Math.floor(Math.random() * max));
+  };
+
+  const animationOut = useCallback(() => {
+    // Do animation out after 1 sec delay.
+    Animated.timing(opacityAnimation, {
+      useNativeDriver: false,
+      toValue: 0,
+      duration: 600,
+      easing: Easing.bezier(0.68, -0.01, 0.31, 0.99),
+    }).start();
+  }, [opacityAnimation]);
+
+  const animationIn = useCallback(() => {
+    // reset values
+    opacityAnimation.setValue(1);
+    scaleAnimation.setValue(0);
+
+    Animated.timing(scaleAnimation, {
+      useNativeDriver: false,
+      toValue: 1,
+      duration: 900,
+      easing: Easing.bezier(0.68, -0.01, 0.31, 0.99),
+      delay: 50 * randomNumber(linesLength),
+    }).start(() => {
+      // starts animation out after current animation is done
+      setTimeout(animationOut, 1000);
+    });
+  }, [scaleAnimation, linesLength, animationOut, opacityAnimation]);
+
+  useEffect(() => {
+    animationIn();
+  }, [animationIn, currentLine]);
 
   return (
-    <Animated.View style={[styles.lyrics]}>
-      <Text>{character}</Text>
+    <Animated.View
+      style={{transform: [{scale: scaleAnimation}], opacity: opacityAnimation}}>
+      <Text style={styles.lyrics}>{character}</Text>
     </Animated.View>
   );
 };
 
 const styles = StyleSheet.create({
   lyrics: {
-    fontSize: 26,
-    fontWeight: '500',
+    fontSize: 32,
     paddingVertical: 4,
     paddingRight: 2,
   },
--- src/randomScale/index.tsx
+++ src/randomScale/index.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Characters from './characters';
 
 const lyrics = ['A simple fold', "That I've told", ' myself to erase'];
@@ -7,11 +7,31 @@ const lyrics = ['A simple fold', "That I've told", ' myself to erase'];
 const RandomScale = () => {
   const [currentLine, setCurrentLine] = useState(0);
 
+  useEffect(() => {
+    // Add timeout to set new lyric active line
+    // will continue the animation into the next lyric line in 8 seconds
+    const timer = setTimeout(() => {
+      if (currentLine < lyrics.length - 1) {
+        setCurrentLine(currentLine + 1);
+      }
+    }, 4000);
+
+    // timeout cleanup
+    return () => clearTimeout(timer);
+  }, [currentLine]);
+
   return (
     <View style={styles.container}>
       <View style={styles.lines}>
         {lyrics[currentLine].split('').map((char, index) => {
-          return <Characters key={index} character={char} />;
+          return (
+            <Characters
+              key={index}
+              character={char}
+              linesLength={lyrics[currentLine].length}
+              currentLine={currentLine}
+            />
+          );
         })}
       </View>
     </View>
@@ -21,7 +41,7 @@ const RandomScale = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',
+    justifyContent: 'center',
     alignItems: 'center',
   },
   lines: {
